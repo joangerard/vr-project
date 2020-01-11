8a69969f180b13d669030e5b70312d685205bd0e
Sebastian Gerard <joan.sebastian.gerard@gmail.com>
2020-01-11 18:17:00 +0100

    change image

--- src/1.getting_started/1.model_loading/model_loading.cpp
+++ src/1.getting_started/1.model_loading/model_loading.cpp
@@ -69,7 +69,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "VR Diskotek", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
